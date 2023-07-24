4bf3a7c3696d729d40c94a42c6c4f732d5334a56
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-07-24 11:36:04 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from './About';
+import ArticleList from './ArticleList';
+import data from '../data/blog';
+
 
 console.log(blogData);
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
-    </div>
+    <>
+    <Header name="OVERREACTED"/>
+    <About image="your_image_url_here" about="Personal blog by Dan Abramov I explain with words and code" />
+      <ArticleList posts={data.posts} />
+     
+    </>
   );
 }
 
--- src/data/blog.js
+++ src/data/blog.js
@@ -1,31 +1,26 @@
 import logo from "../assets/logo";
 
-const blogData = {
-  name: "Underreacted",
-  image: logo,
-  about: "A blog about learning React",
+const data = {
+  name: "Your Blog Name",  
+  about:
+    "Your blog description goes here.",  
   posts: [
     {
-      id: 1,
-      title: "Components 101",
-      date: "December 15, 2020",
-      preview: "Setting up the building blocks of your site",
-      minutes: 5,
+      title: "The wet codebase", 
+      date: "January 5, 2024",
+      preview: "Setting up the building blocks of your site with Goodbye clean code", 
     },
     {
-      id: 2,
-      title: "React Data Flow",
-      date: "December 11, 2020",
-      preview: "Passing props is never passé",
-      minutes: 15,
+      title: "My decade in review",
+      date: "December 20, 2023",  
+      preview: "Passing props is never passé",  
     },
     {
-      id: 3,
-      title: "Function vs Class Components",
-      preview: "React, meet OOJS.",
-      minutes: 47,
+      title: "A new chapter begins",
+      date: "January 5, 2024",  
+      preview: "Embracing React Hooks and Function Components", 
     },
   ],
 };
 
-export default blogData;
+export default data;
